60ba0db78c0ed6bcc64a6e1121076dc7a81e691b
Cristiano <cris.moraes.ds@gmail.com>
2023-07-20 12:25:49 -0300

    fix(sdks/ruby): enable syntax highlight

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ module.exports = {
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
-      additionalLanguages: ['php'],
+      additionalLanguages: ['php', 'ruby'],
     },
     colorMode: {
       defaultMode: 'dark',
